23b7f292e2a2c9849a28a6303cd64bba05d3e400
s20163031 <s20163031@HCS23110.stdlan.hcs.local>
2018-07-09 16:31:07 +0900

    123

--- HSS_DVDRentalPJ/web/WEB-INF/js/scan.js
+++ HSS_DVDRentalPJ/web/WEB-INF/js/scan.js
@@ -9,17 +9,15 @@
 // ---------------------------
 function keydown(e) {
     if(e.keyCode === 13){
-        
-        function keydown2(e) {
-            //alert(String.fromCharCode(e.keyCode));
+        function keydown(e) {
+            if(e.keyCode === 13){
+                document.memberScan.submit();
+            }else{
+                document.getElementById("member").value = document.getElementById("member").value + String.fromCharCode(e.keyCode);
+            }
         }
-        window.onkeydown = keydown2;
-        
-        function keydown3(e) {
-            alert(String.fromCharCode(e.keyCode));
-        }
-        window.onkeydown = keydown3;
-        
+        window.onkeydown = keydown;
     }
+    
 }
 window.onkeydown = keydown;

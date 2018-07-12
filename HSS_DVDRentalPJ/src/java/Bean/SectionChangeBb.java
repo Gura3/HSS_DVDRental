ffc4aeeaf71c70868d4bce50532c57491d0f7a4a
s20163002 <s20163002@HCS23103.stdlan.hcs.local>
2018-07-12 14:46:46 +0900

    フォルダ移動

---
+++ HSS_DVDRentalPJ/src/java/Bean/SectionChangeBb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author s20163002
+ */
+@Named
+@RequestScoped
+public class SectionChangeBb {
+    public String next(){
+        return "/pages/dvd_information_change/section_change.xhtml";
+    }
+}
